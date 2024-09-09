1be25d5596da6b96a0432a1d706cc8f6a7475439
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-09-09 20:41:15 +0200

    better DB self-explainatory view

--- lib/database.rb
+++ lib/database.rb
@@ -7,11 +7,11 @@ def db_dump(db: nil)
   db ||= Sequel.sqlite(ENV["DATABASE_NAME"])
 
   puts("== DB DUMP ==")
-  puts "1. Products:"
-  puts(db[:products].all)
-  %w{ orders order_items products customers}.each do |table_name|
+  #puts "1. Products:"
+  #puts(db[:products].all)
+  %w{ orders order_items products customers}.sort.each_with_index do |table_name, ix|
     table_sym = table_name.to_sym
-    puts "X. #{table_sym}:"
+    puts "#{ix+1}. $DB[:#{table_sym}].all "
     puts(db[table_sym].all)
   end
 
